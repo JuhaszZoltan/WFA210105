1d2bada6ff5798808ec9275e6e632305557f1a84
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-02-01 08:51:39 +0100

    add 12_12

---
+++ feladatok12_12/FrmMain.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace feladatok12_12
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSzamol = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.lblX1 = new System.Windows.Forms.Label();
+            this.lblX2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtA.Location = new System.Drawing.Point(17, 12);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(40, 30);
+            this.txtA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(63, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X^2 +";
+            // 
+            // btnSzamol
+            // 
+            this.btnSzamol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSzamol.Location = new System.Drawing.Point(12, 60);
+            this.btnSzamol.Name = "btnSzamol";
+            this.btnSzamol.Size = new System.Drawing.Size(304, 39);
+            this.btnSzamol.TabIndex = 2;
+            this.btnSzamol.Text = "számol";
+            this.btnSzamol.UseVisualStyleBackColor = true;
+            this.btnSzamol.Click += new System.EventHandler(this.BtnSzamol_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(176, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(271, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "= 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "X1 =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "X2 =";
+            // 
+            // txtB
+            // 
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtB.Location = new System.Drawing.Point(130, 12);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(40, 30);
+            this.txtB.TabIndex = 0;
+            // 
+            // txtC
+            // 
+            this.txtC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtC.Location = new System.Drawing.Point(225, 12);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(40, 30);
+            this.txtC.TabIndex = 0;
+            // 
+            // lblX1
+            // 
+            this.lblX1.AutoSize = true;
+            this.lblX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblX1.Location = new System.Drawing.Point(72, 116);
+            this.lblX1.Name = "lblX1";
+            this.lblX1.Size = new System.Drawing.Size(0, 25);
+            this.lblX1.TabIndex = 1;
+            // 
+            // lblX2
+            // 
+            this.lblX2.AutoSize = true;
+            this.lblX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblX2.Location = new System.Drawing.Point(72, 167);
+            this.lblX2.Name = "lblX2";
+            this.lblX2.Size = new System.Drawing.Size(0, 25);
+            this.lblX2.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 206);
+            this.Controls.Add(this.btnSzamol);
+            this.Controls.Add(this.lblX2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "FrmMain";
+            this.Text = "Másodfokú-megoldó";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSzamol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label lblX1;
+        private System.Windows.Forms.Label lblX2;
+    }
+}
+
---
+++ feladatok12_12/FrmMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace feladatok12_12
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSzamol_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(txtA.Text);
+            float b = float.Parse(txtB.Text);
+            float c = float.Parse(txtC.Text);
+
+            float d = (b * b) - (4 * a * c);
+
+            if (a == 0) lblX1.Text = $"{-c / b}";
+            else if(d == 0) lblX1.Text = $"{-b / (2 * a)}";
+            else if (d < 0) lblX1.Text = lblX2.Text = $"nem valós szám";
+            else
+            {
+                lblX1.Text = $"{(-b + Math.Sqrt(d)) / (2 * a)}";
+                lblX2.Text = $"{(-b - Math.Sqrt(d)) / (2 * a)}";
+            }
+
+
+        }
+    }
+}
